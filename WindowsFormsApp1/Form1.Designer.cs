6c51c2a03816ac6d4152c9125b60b2daa5f0c984
Andru <Andru@ANDRUSEASUS>
2024-12-08 19:13:52 +0200

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.achievements = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Rblxdoorsnewfont", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Peru;
+            this.button1.Location = new System.Drawing.Point(302, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 125);
+            this.button1.TabIndex = 0;
+            this.button1.TabStop = false;
+            this.button1.Text = "Click!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.label1.Location = new System.Drawing.Point(365, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 69);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // achievements
+            // 
+            this.achievements.AutoSize = true;
+            this.achievements.Location = new System.Drawing.Point(3, 22);
+            this.achievements.Name = "achievements";
+            this.achievements.Size = new System.Drawing.Size(220, 13);
+            this.achievements.TabIndex = 2;
+            this.achievements.Text = "You haven\'t unlocked any achievements yet!";
+            this.achievements.Click += new System.EventHandler(this.achievements_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(606, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(606, 52);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(693, 385);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.achievements);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimumSize = new System.Drawing.Size(553, 264);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DOORS Clicker 2024";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label achievements;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button resetButton;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+
+        private SoundPlayer AchievementSound;
+
+        private int ClickCount = 0; // Class-level variable to hold the click count
+
+        private bool Tricky = false;
+        private int TrickyLevel = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Initialize the SoundPlayer using a relative path
+            AchievementSound = new SoundPlayer("achievement.wav");
+
+            async void MoveButton()
+            {
+                while (true)
+                {
+                    if (Tricky)
+                    {
+                        button1.Location = new Point(
+                            random.Next(button1.Size.Width, this.ClientSize.Width - button1.Size.Width),
+                            random.Next(button1.Size.Height, this.ClientSize.Height - button1.Size.Height)
+                        );
+                    }
+
+                    // Delay to control the speed of the loop and prevent UI freezing
+                    await Task.Delay((int)(10 * 1000 / TrickyLevel));
+                }
+            }
+
+            MoveButton();
+        }
+
+        // Add a private clickCount variable to track the count
+        private int clickCount = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Increment the click count by 1
+            clickCount++;
+
+            // Check if the click count is a multiple of 100
+            if (clickCount % 100 == 0)
+            {
+                if (achievements.Text == "You haven't unlocked any achievements yet!")
+                {
+                    achievements.Text = "You got " + clickCount + " clicks \n";
+                    button1.Text = "Click, Click, Click!";
+                    AchievementSound.Play();
+                }
+                else
+                {
+                    achievements.Text += "You got " + clickCount + " clicks \n";
+                    AchievementSound.Play();
+                    Tricky = true;
+                    TrickyLevel += 1;
+                }
+            }
+
+            // Update label1 with the new click count
+            label1.Text = clickCount.ToString();
+            label1.Size = new Size(130, 23);
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string saveFilePath = "clickCount.txt"; // Define the path for the save file
+
+            if (File.Exists(saveFilePath)) // Check if the file exists
+            {
+                try
+                {
+                    string savedClickCount = File.ReadAllText(saveFilePath); // Read the saved count
+                    if (int.TryParse(savedClickCount, out clickCount)) // Try to parse it as an integer
+                    {
+                        label1.Text = clickCount.ToString(); // Set the label to the saved count
+                    }
+                    else
+                    {
+                        MessageBox.Show("Invalid saved data. Starting count from 0."); // Handle invalid data
+                        clickCount = 0; // Reset to 0 if invalid
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error loading click count: " + ex.Message); // Error handling
+                }
+            }
+        }
+
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void shopButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string saveFilePath = "clickCount.txt"; // Define the save file path
+
+            try
+            {
+                File.WriteAllText(saveFilePath, clickCount.ToString()); // Save the click count to the file
+                MessageBox.Show("Click count saved successfully!"); // Confirm save
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error saving click count: " + ex.Message); // Error handling
+            }
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            // Reset the click count to 0
+            clickCount = 0;
+
+            // Update the label to reflect the reset count
+            label1.Text = clickCount.ToString();
+
+            // Clear any achievements text or reset it to the default state
+            achievements.Text = "You haven't unlocked any achievements yet!";
+
+            // Optionally, you could also reset any other UI elements related to the game state
+            button1.Text = "Start Clicking!"; // Reset button text if needed
+
+            // You may also choose to clear the saved data
+            string saveFilePath = "clickCount.txt"; // Define the save file path
+            if (File.Exists(saveFilePath))
+            {
+                File.Delete(saveFilePath); // Delete the saved file to reset the progress
+            }
+
+            MessageBox.Show("Click count has been reset!"); // Confirm reset
+        }
+
+        private void achievements_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
